774368f89179a4975b246c534bef13abc7f4dac9
nfnty <git@nfnty.se>
2015-05-14 18:34:08 +0200

    Refactoring

--- hostsupdate.py
+++ hostsupdate.py
@@ -5,117 +5,113 @@ import requests
 import os
 import re
 import time
-import subprocess
 import json
 import sys
 
-DESTDIR = '/etc/hosts.d'
-CONFDIR = '/etc/hostsupdate'
+DESTDIR = '/srv/docker/powerdns-recursor/config/blacklist'
+CONFIGDIR = '/etc/hostsupdate'
 
-def load_conf(conf_file):
-    ''' load json config file '''
+DOMAIN_REGEX = re.compile(
+    r'^' +
+    r'(?:[A-Za-z0-9-_]{1,63}\.)*' +
+    r'(?:(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)' +
+    r'(?:(?!-)[A-Za-z0-9-]{2,63}(?<!-))' +
+    r'$'
+)
+
+def config_load(path):
+    ''' Load json config file '''
     try:
-        json_data = json.load(open(os.path.join(CONFDIR, conf_file)))
+        json_data = json.load(open(path))
     except (OSError, ValueError) as error:
-        print('Error: ' + conf_file)
+        print('Error: ' + path)
         print(str(error))
         sys.exit(1)
     return json_data
 
-def fetch_harvest(url):
-    ''' fetch and harvest domains from remote text '''
+def remote_parse(url):
+    ''' Get remote and parse lines into array '''
     try:
         req = requests.get(url, timeout=30)
     except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as error:
         print(str(error))
         return False
 
-    harvested_domains = []
+    lines = []
     for line in req.text.splitlines():
         line = line.partition('#')[0]
         if line.strip():
-            harvested_domains.append(line.split()[-1])
+            lines.append(line.split()[-1])
 
-    return harvested_domains
+    return lines
 
-def encode_check_domain(domain, regex, invalid, excluded):
-    ''' encode and check domain '''
+def domain_encode(domain):
+    ''' Encode domain '''
     try:
         encoded = domain.encode('idna').decode('UTF-8')
     except UnicodeError as error:
-        print('Error: "' + domain + '"')
+        print('Encode error: ' + domain)
         print(str(error))
         return False
+
     if domain != encoded:
-        print('Before: "' + domain + '"')
-        print('After: "' + encoded + '"')
-
-    if len(encoded) > 253 \
-            or encoded in invalid \
-            or not regex.fullmatch(encoded):
-        print('Invalid: "' + encoded + '"')
-        return False
-
-    if encoded in excluded:
-        print('Excluded: "' + encoded + '"')
-        return False
+        print('Before: ' + domain)
+        print('After: ' + encoded)
 
     return encoded
 
+def domain_validate(domain, invalid, excluded):
+    ''' Validate domain '''
+    if len(domain) > 253:
+        print('Domain too long')
+        return False
+
+    if domain in invalid:
+        print('Invalid: ' + domain)
+        return False
+
+    if not DOMAIN_REGEX.fullmatch(domain):
+        print('Regex error: ' + domain)
+        return False
+
+    if domain in excluded:
+        print('Excluded: ' + domain)
+        return False
+
+    return True
+
 def main():
-    ''' main func '''
+    ''' Main '''
 
-    sources = load_conf('sources.json')['sources']
+    sources = config_load(os.path.join(CONFIGDIR, 'sources.json'))['sources']
     sources.reverse()
-    invalid_domains = load_conf('invalid.json')['domains']
-    excluded_domains = load_conf('excluded.json')['domains']
-
-    domain_regex = re.compile(
-        r'^' +
-        r'(?:[A-Za-z0-9-_]{1,63}\.)*' +
-        r'(?:(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)' +
-        r'(?:(?!-)[A-Za-z0-9-]{2,63}(?<!-))' +
-        r'$'
-    )
+    domains_invalid = config_load(os.path.join(CONFIGDIR, 'invalid.json'))['domains']
+    domains_excluded = config_load(os.path.join(CONFIGDIR, 'excluded.json'))['domains']
 
     while sources:
-        sources_length = len(sources)
-        for _ in range(sources_length):
+        for _ in range(len(sources)):
             source = sources.pop()
             print('### ' + source['name'] + ' ###')
 
-            harvested_domains = fetch_harvest(source['url'])
-            if not harvested_domains:
+            lines = remote_parse(source['url'])
+            if not lines:
                 sources.insert(0, source)
                 continue
 
             try:
-                with open(os.path.join(DESTDIR, source['name']), 'w') as hosts_file:
-                    for domain in harvested_domains:
-                        domain_encoded = encode_check_domain(
-                            domain, domain_regex, invalid_domains, excluded_domains
-                        )
-                        if domain_encoded:
-                            hosts_file.write('0.0.0.0 ' + domain_encoded + '\n')
+                with open(os.path.join(DESTDIR, source['name']), 'w') as blacklist:
+                    for line in lines:
+                        domain = domain_encode(line)
+                        if not domain:
+                            continue
+                        if domain_validate(domain, domains_invalid, domains_excluded):
+                            blacklist.write(domain + '\n')
             except OSError as error:
                 print(str(error))
                 sys.exit(1)
 
-        if sources_length != len(sources):
-            try:
-                subprocess.check_call([
-                    '/usr/bin/pkill',
-                    '--uid', 'dnsmasq',
-                    '--group', 'dnsmasq',
-                    '--exact',
-                    '--signal', 'SIGHUP',
-                    'dnsmasq',
-                ])
-            except subprocess.CalledProcessError as error:
-                print(str(error))
-
         if sources:
-            print('Connection problem: Sleeping for 1 hour')
+            print('Error: Sleeping for 1 hour')
             time.sleep(3600)
 
 if __name__ == '__main__':
